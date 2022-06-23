49a56c042a4a674369ff742df0cb86688589f143
DestinyIJ <ighedodestiny11@gmail.com>
2022-06-23 07:34:36 +0100

    Auth issues rectified

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']); // route to logout
+Route::post('/login', [AuthController::class, 'login']); // route to login
 Route::post('/logout', [AuthController::class, 'logout']); // route to logout
 
